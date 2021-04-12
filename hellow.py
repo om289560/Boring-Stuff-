6d730eaecf6285cf44a2146ee1a260cfa54eb375
Oscar Miranda <omiranda0058@gmail.com>
2021-04-11 19:58:50 -0500

    latest update

--- hellow.py
+++ hellow.py
@@ -6,7 +6,7 @@ myName = input()
 print ('it is good to meet you, ' + myName)
 print ('the length of your name is:')
 print (len(myName))
-print ('waht is your age?') # ask for their age
+print ('what is your age?') # ask for their age
 myAge = input()
 print ('You will be ' + str(int(myAge) + 1) + ' in a year.')
 
