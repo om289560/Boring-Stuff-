586e08f2ef495dcfd4bf33655d83d2890848bedb
Oscar Miranda Jr <50378464+om289560@users.noreply.github.com>
2021-04-11 19:45:11 -0500

    Add files via upload

---
+++ hellow.py
@@ -0,0 +1,12 @@
+# This program says hellow and ask for my name
+
+print ('hellow world!')
+print ('what is your name?') #ask for their name
+myName = input()
+print ('it is good to meet you, ' + myName)
+print ('the length of your name is:')
+print (len(myName))
+print ('waht is your age?') # ask for their age
+myAge = input()
+print ('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
